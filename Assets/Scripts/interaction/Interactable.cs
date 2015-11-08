d28fa814955d6cdbf48d99186951db0e52333e3e
cebollinos11 <cebollinos@gmail.com>
2015-11-08 14:50:13 +0100

    interactable basic

---
+++ Assets/Scripts/interaction/Interactable.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable : MonoBehaviour {
+
+    public delegate void HighlightHandler();
+    public delegate void DeHighlightHandler();
+    public delegate void DoInteractButtonDownHandler();
+    public delegate void DoInteractButtonUpHandler();
+
+    public event HighlightHandler OnHighLight;
+    public event HighlightHandler OnDeHighLight;
+    public event HighlightHandler OnDoInteractButtonUp;
+    public event HighlightHandler OnDoInteractButtonDown;
+
+    public virtual void DoHighlight() {
+        //GuiManager.DisplayInfoText("Selected " + gameObject.name);
+        if(OnHighLight!=null)
+        OnHighLight();
+    }
+    public virtual void DoDeHighlight() {
+        if (OnDeHighLight != null)
+        OnDeHighLight();
+    }
+    public virtual void DoInteractButtonDown() {
+        Debug.Log("PRESS");
+        if (OnDoInteractButtonUp != null)
+        OnDoInteractButtonUp();
+
+    }
+    public virtual void DoInteractButtonUp() {
+        Debug.Log("BUTTON UP");
+        if (OnDoInteractButtonDown != null)
+        OnDoInteractButtonDown();
+    }
+}
