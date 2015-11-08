7cb09ced0d19c9e38b25e1923932f07dda720bb3
cebollinos11 <cebollinos@gmail.com>
2015-11-08 17:11:22 +0100

    fixed door prefab

---
+++ Assets/Scripts/CeilingBuilder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CeilingBuilder : MonoBehaviour {
+
+    public float height;
+
+	// Use this for initialization
+	void Start () {
+
+        GameObject instance = Instantiate(Resources.Load("mapgen/roomParts/Ceiling", typeof(GameObject))) as GameObject;
+        instance.transform.position = transform.position+new Vector3(0,height,0);
+        instance.transform.localScale = transform.localScale;
+        instance.transform.rotation = transform.rotation;
+        instance.transform.Rotate(0, 0, 180, Space.Self);    
+        
+	    
+	}
+	
+	
+}
