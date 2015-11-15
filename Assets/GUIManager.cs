92c3e21e2f73d446464ade98714445367e4d4a97
cebollinos11 <cebollinos@gmail.com>
2015-11-15 14:44:25 +0100

    terminal can only be hacked once

---
+++ Assets/GUIManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIManager : Singleton<GUIManager> {
+
+    public Text interactableLabelText;
+
+	// Use this for initialization
+	void Start () {
+
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/interaction/Interactable_HackTerminal.cs
+++ Assets/Scripts/interaction/Interactable_HackTerminal.cs
@@ -9,20 +9,30 @@ public class Interactable_HackTerminal : Interactable {
 
     public override void DoHighlight() {
         base.DoHighlight();
+        GUIManager.Instance.interactableLabelText.text = "Press E to hack the terminal";
     }
     public override void DoDeHighlight() {
         base.DoDeHighlight();
+        GUIManager.Instance.interactableLabelText.text = "";
     }
     public override void DoInteractButtonDown() {
         base.DoInteractButtonDown();
-
-        Debug.Log("SHIT MAN");
+               
         GameController.Instance.ShutDown();
-
-        
+        disableTerminal();        
     }
     public override void DoInteractButtonUp() {
         base.DoInteractButtonUp();
+    }
+
+    private void disableTerminal(){
+
+       
+        for (int i = 0; i < transform.parent.childCount; i++)
+        {
+            transform.parent.GetChild(i).gameObject.layer = 0;
+        }
+    
     }
 	
 }
