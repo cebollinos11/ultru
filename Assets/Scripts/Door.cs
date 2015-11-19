3c5f6b498157d668a7466d2754344debcd52217a
cebollinos11 <cebollinos@gmail.com>
2015-11-19 14:15:05 +0100

    added door sound and fixed flashlight

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,9 +6,17 @@ public class Door : MonoBehaviour {
 	[SerializeField] Animation doorAnim;
 	public Transform zero;
 
+    AudioSource myAudioSource;
+
+    void Start() {
+        myAudioSource = GetComponent<AudioSource>();
+    }
+    
 
 	void OnTriggerEnter (Collider col) {
         GetComponentInParent<Animator>().SetBool("PlayerNearby", true);
+        if (!myAudioSource.isPlaying)
+            myAudioSource.Play();
 	}
 	
 	void OnTriggerExit (Collider obj) {
